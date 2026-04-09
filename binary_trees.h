058bb21bcaab9a83ac1ddeecab553438b51a9a0b
Mr.Robot <sfecom.31000@gmail.com>
2026-04-09 09:59:29 +0200

    Add binary_tree_leaves function prototype

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -43,5 +43,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+size_t binary_tree_leaves(const binary_tree_t *tree)
 
 #endif
